5f9d6213c986d35ccc468291284c177af4363f78
Valentin Manea <valy@mrs.ro>
2017-12-03 22:52:11 +0200

    Add temp proxy

---
+++ read_temp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+import serial
+import re
+import requests
+
+def basic_header():
+	return {
+		"Content-type": "text/plain"}
+
+def put_status(name, teme):
+    """ Put a status update to OpenHAB  key is item, value is state """
+    url = 'http://openhab.lan:8081/rest/items/%s/state' % (name)
+    req = requests.put(url, data=temp, headers=basic_header())
+    if req.status_code != requests.codes.ok:
+        req.raise_for_status()     
+
+with serial.Serial('/dev/ttyUSB0', 4800, timeout=80) as ser:
+	while True:
+		line = ser.readline()   # read a '\n' terminated line
+		if line == []:
+		    continue
+		result = re.match("Extracted temp: (\d+)", line)
+		if result:
+			temp = float(result.group(1))
+			temp /= 10
+			print(temp)
+			temp = "%.1f" % (temp)
+			put_status('Temperature_Balcony', temp)
+		print(line)
+	ser.close()
